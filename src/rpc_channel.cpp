a6b02d05b6feb918276a181124e4cc6c7f44766e
ysj1173886760 <1173886760@qq.com>
2022-04-14 21:08:34 +0800

    implement rpc channel

--- src/rpc_channel.cpp
+++ src/rpc_channel.cpp
@@ -59,5 +59,47 @@ void TinyChannel::CallMethod(const ::google::protobuf::MethodDescriptor* method,
                             const ::google::protobuf::Message* request,
                             ::google::protobuf::Message* response,
                             ::google::protobuf::Closure* done) {
+    // serialize the request message
+    std::string request_buffer = request->SerializeAsString();
 
+    RpcMeta rpc_meta;
+    rpc_meta.set_service_name(method->service()->name());
+    rpc_meta.set_method_name(method->name());
+    rpc_meta.set_data_size(request_buffer.size());
+
+    std::string meta_buffer = rpc_meta.SerializeAsString();
+
+    int rpc_meta_size = meta_buffer.size();
+
+    std::string buffer;
+    // serialize the rpc_meta_size
+    buffer += std::string((char *)&rpc_meta_size, sizeof(int));
+    // append meta data
+    buffer += meta_buffer;
+    // append request data, we don't need to append the length of request_buffer
+    // since it has been stored in rpc_meta
+    buffer += request_buffer;
+
+    LOG_INFO("sending rpc request");
+    LOG_INFO("service name: %s", rpc_meta.service_name().c_str());
+    LOG_INFO("method name: %s", rpc_meta.method_name().c_str());
+    LOG_INFO("meta size: %d", rpc_meta_size);
+    LOG_INFO("request size: %d", rpc_meta.data_size());
+
+    // since we are using bio here, so it's safe to put send buffer on stack
+    send(socket_, buffer.c_str(), buffer.size(), 0);
+
+    // receive response
+    // first 4 byte is response size, then comes response data
+    char resp_data_size[sizeof(int)];
+    // bio here, recv the header
+    recv(socket_, resp_data_size, 4, MSG_WAITALL);
+    
+    int resp_len = *((int *)resp_data_size);
+    std::string resp_buffer;
+    resp_buffer.resize(resp_len);
+
+    // bio here, recv the data
+    recv(socket_, (void *)resp_buffer.c_str(), resp_len, MSG_WAITALL);
+    response->ParseFromString(resp_buffer);
 }
